a299d48f9c2372511f8b1c26b0043c648231dffe
Elvira <elvira.n.ivanova@gmail.com>
2023-10-23 12:04:16 +0300

    Installed DaisyUI and added styling

---
+++ app/components/AddToCart.tsx
@@ -0,0 +1,13 @@
+"use client";
+import React from "react";
+
+const AddToCart = () => {
+  return (
+    <div>
+      <button onClick={() => console.log("Click")}>Add to Cart</button>
+      <button className="btn btn-primary"> add to cart</button>
+    </div>
+  );
+};
+
+export default AddToCart;
