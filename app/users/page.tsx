6509b5f7b77a2a00f72fb7073064be735d9ef3dd
Elvira <elvira.n.ivanova@gmail.com>
2023-10-24 14:50:58 +0300

    Added dynamic routes

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,37 +1,13 @@
-import React from 'react'
-
-interface User {
-  id:number;
-  name:string
-  email:string
-}
+import React from "react";
+import UserTable from "./UserTable";
 
 const UsersPage = async () => {
-
-  const res = await fetch('https://jsonplaceholder.typicode.com/users', 
-  {cache: 'no-store'})
-  const users:User[] = await res.json();
-
-
   return (
-    
     <>
-    <h1>Users</h1>
-    
-    <table className='table table-bordered'>
-      <thead>
-        <tr>
-          <th>Name</th>
-          <th>Email</th>
-        </tr>
-      </thead>
-      <tbody>
-        {users.map((user) => <tr key={user.id}><td>{user.name}</td>
-        <td>{user.email}</td></tr>)}
-      </tbody>
-    </table>
+      <h1>Users</h1>
+      <UserTable />
     </>
-  )
-}
+  );
+};
 
-export default UsersPage
+export default UsersPage;
