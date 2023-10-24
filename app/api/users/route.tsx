b018b74d3629b9a11c0f93e1dbb459ec2ef9bb09
Elvira <elvira.n.ivanova@gmail.com>
2023-10-24 17:14:52 +0300

    Added zod

---
+++ app/api/users/route.tsx
@@ -0,0 +1,23 @@
+import { NextRequest, NextResponse } from "next/server";
+import { schema } from "./schema";
+
+
+export const GET = (request: NextRequest) => {
+  return NextResponse.json([
+    { id: 1, name: "Elvira" },
+    { id: 2, name: "Lvra" },
+  ]);
+};
+
+export const POST = async(request:NextRequest)=> {
+    const body = await request.json()
+
+    const validation = schema.safeParse(body);
+    if(!validation.success){
+        return NextResponse.json(validation.error.errors, {
+            status:400
+        })
+    }
+
+    return NextResponse.json({id:1, name: body.name}, {status:201})
+}
